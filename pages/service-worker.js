6ec89c9f8f902ce7bdd5e6d99cbe9365ca6dbdc9
ZippCodder <deonisawewsome777@gmail.com>
2021-09-28 23:44:42 +0000

    .

--- pages/service-worker.js
+++ pages/service-worker.js
@@ -1,7 +1,3 @@
 self.addEventListener("fetch",(e) => {
- if (e.request.url === "https://zippcodder.github.io/inkpad/pages/index.html") {
-	e.respondWith(new Promise((res,rej) => { 
-   res(new Response("Herh",{status: 200}));
- }));
- }
+ // handle cache and shit...
 });
